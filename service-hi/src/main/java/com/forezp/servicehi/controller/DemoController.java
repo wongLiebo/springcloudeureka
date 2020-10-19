f91e0cb6a2b33c7ccd96f7c0c8515ec3325debfc
wongLiebo <job_twang@163.com>
2020-10-19 09:37:24 +0800

    springcloudeureka  demo实例提交

---
+++ service-hi/src/main/java/com/forezp/servicehi/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.forezp.servicehi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
